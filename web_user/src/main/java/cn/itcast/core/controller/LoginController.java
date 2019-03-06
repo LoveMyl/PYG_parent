5b90ad60f9aa5850c13b78600adbe0c455c0c93d
LJX <LJX@163.com>
2019-03-06 16:24:47 +0800

    提交:  实现页面我的订单跳转功能

---
+++ web_user/src/main/java/cn/itcast/core/controller/LoginController.java
@@ -0,0 +1,21 @@
+package cn.itcast.core.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: pyg_parent
+ * @author: 刘家兴
+ * @create: 2019-03-06 16:18
+ * @version: 1.0
+ **/
+@RestController
+@RequestMapping("login")
+public class LoginController {
+
+
+    public String loginName(){
+        return null;
+    }
+
+}
