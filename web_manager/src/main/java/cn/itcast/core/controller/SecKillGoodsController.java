8bc35b2e35138e35f59cd277f0bd1af715085942
gelanqing <1148295896@qq.com>
2019-03-09 12:18:10 +0800

    秒杀订单查询

---
+++ web_manager/src/main/java/cn/itcast/core/controller/SecKillGoodsController.java
@@ -0,0 +1,29 @@
+package cn.itcast.core.controller;
+
+import cn.itcast.core.pojo.entity.PageResult;
+import cn.itcast.core.pojo.entity.Result;
+import cn.itcast.core.pojo.good.Goods;
+import cn.itcast.core.service.GoodsService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/seckillgoods")
+public class SecKillGoodsController {
+
+
+/*
+
+    @RequestMapping("/updateStatus")
+
+    @RequestMapping("/search")
+    public List<Goods> findAll() {
+        return goodsService.findAll();
+    }
+*/
+}
